f7e497e93eca56d9c9cf155bd63c90776a670ed2
Matthew D Jones <matthew.d.jones@tessella.com>
2019-04-24 12:33:14 +0100

    Refactor recipe and upgrade to 1.10.5

--- conanfile.py
+++ conanfile.py
@@ -1,198 +1,86 @@
 import os
-import shutil
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.errors import ConanException
-
-
-class ConfigurationException(Exception):
-    pass
+from conans import ConanFile, tools, CMake
 
 
 class Hdf5Conan(ConanFile):
     name = "hdf5"
-    sha256 = "bfec1be8c366965a99812cf02ddc97e4b708c1754fccba5414d4adccdc073866"
-
-    version_number = "1.10.2"
-    version = "1.10.2-dm2"
+    version = "1.10.5"
     description = "HDF5 C and C++ libraries"
-    license = "https://support.hdfgroup.org/ftp/HDF5/releases/COPYING"
     url = "https://github.com/ess-dmsc/conan-hdf5"
-    exports = ["files/CHANGES", "files/HDF5options.cmake"]
-    settings = "os", "compiler", "build_type", "arch"
-    requires = "zlib/1.2.11@conan/stable"
+    homepage = "https://www.hdfgroup.org/"
+
+    license = "https://support.hdfgroup.org/ftp/HDF5/releases/COPYING"
+
+    exports = ["LICENSE.md", "files/CHANGES"]
+
+    exports_sources = ["CMakeLists.txt"]
+    generators = ["cmake", "virtualbuildenv"]
+
+    settings = "os", "arch", "compiler", "build_type"
+    
     options = {
         "cxx": [True, False],
         "shared": [True, False],
         "parallel": [True, False]
     }
+    
     default_options = (
         "cxx=True",
         "shared=False",
         "parallel=False",
         "zlib:shared=False"
     )
-    generators = "virtualbuildenv"
+    
+    short_paths = True
 
-    folder_name = "hdf5-%s" % version_number
-    windows_source_folder = "CMake-" + folder_name
-    archive_name = "%s.tar.gz" % folder_name
-    windows_archive_name = "%s.zip" % windows_source_folder
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
+
+    requires = "zlib/1.2.11@conan/stable"
+
+    def source(self):
+        minor_version = ".".join(self.version.split(".")[:2])
+        tools.get("https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-{0}/hdf5-{1}/src/hdf5-{1}.tar.gz"
+                  .format(minor_version, self.version))
+        os.rename("hdf5-{0}".format(self.version), self.source_subfolder)
 
     def configure(self):
         if self.options.cxx and self.options.parallel:
             msg = "The cxx and parallel options are not compatible"
             raise ConfigurationException(msg)
+        self.options["zlib"].shared = self.options.shared
 
-    def source(self):
-        if tools.os_info.is_windows:
-            tools.download(
-                "https://www.hdfgroup.org/package/source-cmake-windows-2/?wpdmdl=11820",
-                self.windows_archive_name
-            )
-            tools.unzip(self.windows_archive_name)
-            os.unlink(self.windows_archive_name)
-        else:
-            tools.download(
-                "https://www.hdfgroup.org/package/source-gzip-2/?wpdmdl=11810",
-                self.archive_name
-            )
-            tools.check_sha256(self.archive_name, self.sha256)
-            tools.unzip(self.archive_name)
-            os.unlink(self.archive_name)
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(build_folder=self.build_subfolder, defs={
+            "HDF5_BUILD_EXAMPLES": False,
+            "HDF5_BUILD_TOOLS": True,
+            "HDF5_BUILD_HL_LIB": False,
+            "HDF5_BUILD_CPP_LIB": self.options.cxx,
+            "HDF5_ENABLE_Z_LIB_SUPPORT": True,
+            "HDF5_ENABLE_PARALLEL": self.options.parallel
+        })
+        return cmake
 
     def build(self):
-        configure_args = [
-            "--prefix=",
-            "--enable-hl",
-            "--disable-sharedlib-rpath"
-        ]
-
-        if self.settings.build_type == "Debug":
-            configure_args.append("--enable-build-mode=debug")
-
-        if self.options.cxx:
-            configure_args.append("--enable-cxx")
-
-        if self.options.shared:
-            configure_args.append("--enable-shared")
-            configure_args.append("--disable-static")
-        else:
-            configure_args.append("--disable-shared")
-            configure_args.append("--enable-static")
-
         if self.options.parallel:
             if os.environ.get("CC") is None:
                 os.environ["CC"] = os.environ.get("MPICC", "mpicc")
             if os.environ.get("CXX") is None:
                 os.environ["CXX"] = os.environ.get("MPICXX", "mpic++")
-            configure_args.append("--enable-parallel")
-
-        if tools.os_info.is_linux and self.options.shared:
-            val = os.environ.get("LDFLAGS", "")
-            os.environ["LDFLAGS"] = val + " -Wl,-rpath='$$ORIGIN/../lib'"
-
-        if tools.os_info.is_windows:
-            cwd = os.getcwd()
-            os.chdir(os.path.join(self.source_folder, self.windows_source_folder))
-
-            # Override build settings using our own options file
-            shutil.copyfile(
-                os.path.join(self.source_folder, "files", "HDF5options.cmake"),
-                "HDF5options.cmake"
-            )
-
-            static_option = "No" if self.options.shared else "Yes"
-            try:
-                self.run("ctest -S HDF5config.cmake,BUILD_GENERATOR=VS201564,STATIC_ONLY=%s -C %s -V -O hdf5.log" % (static_option, self.settings.build_type))
-            except ConanException:
-                # Allowed to "fail" on having no tests to run, because we purposely aren't building the tests
-                pass
-
-            install_package_name = "HDF5-%s-win64" % self.version_number
-            install_package = "HDF5-%s-win64.zip" % self.version_number
-            os.chdir("build")
-            tools.unzip(install_package)
-            os.unlink(install_package)
-            shutil.copytree(install_package_name, os.path.join("..", "..", "install"))
-            os.chdir(os.path.join("..", "..", "install"))
-
-        else:
-            os.mkdir("install")
-
-            env_build = AutoToolsBuildEnvironment(self)
-            env_build.configure(
-                configure_dir=self.folder_name,
-                args=configure_args
-            )
-
-            if tools.os_info.is_macos and self.options.shared:
-                tools.replace_in_file(
-                    r"./libtool",
-                    r"-install_name \$rpath/\$soname",
-                    r"-install_name @rpath/\$soname"
-                )
-
-            env_build.make()
-
-            cwd = os.getcwd()
-            destdir = os.path.join(cwd, "install")
-            env_build.make(args=["install", "DESTDIR="+destdir])
-
-            # The paths in the HDF5 compiler wrapper are hard-coded, so
-            # substitute the prefix by a variable named H5CC_PREFIX to be
-            # passed to it. The compiler wrapper can be called h5cc or h5pcc.
-            if self.options.parallel:
-                hdf5_compiler_wrapper = os.path.join(destdir, "bin", "h5pcc")
-            else:
-                hdf5_compiler_wrapper = os.path.join(destdir, "bin", "h5cc")
-
-            tools.replace_in_file(
-                hdf5_compiler_wrapper,
-                'prefix=""',
-                'prefix="$(cd "$( dirname "$0" )" && pwd)/.."'
-            )
-
-            if tools.os_info.is_macos and self.options.shared:
-                self._add_rpath_to_executables(os.path.join(destdir, "bin"))
-
-            os.chdir(self.folder_name)
-            os.rename("COPYING_LBNL_HDF5", "LICENSE.hdf5_LBNL")
-            shutil.copyfile(
-            os.path.join(self.source_folder, "files", "CHANGES"),
-            "CHANGES.hdf5"
-        )
-
-        os.rename("COPYING", "LICENSE.hdf5")
-        os.chdir(cwd)
-
-    def _add_rpath_to_executables(self, path):
-        executables = [
-            "gif2h5", "h52gif", "h5clear", "h5copy", "h5debug", "h5diff",
-            "h5dump", "h5format_convert", "h5import", "h5jam", "h5ls",
-            "h5mkgrp", "h5perf_serial", "h5repack", "h5repart", "h5stat",
-            "h5unjam", "h5watch"
-        ]
-        cwd = os.getcwd()
-        os.chdir(path)
-        for e in executables:
-            cmd = "install_name_tool -add_rpath {0} {1}".format(
-                "@executable_path/../lib", e
-            )
-            os.system(cmd)
-
-        os.chdir(cwd)
+        
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
-        self.copy("*", dst="bin", src="install/bin")
-        self.copy("*", dst="include", src="install/include")
-        self.copy("*", dst="lib", src="install/lib")
-        if not tools.os_info.is_windows:
-            self.copy("LICENSE.*", src=self.folder_name)
-            self.copy("CHANGES.*", src=self.folder_name)
+        cmake = self._configure_cmake()
+        cmake.install()
+        self.copy("COPYING", src=self.source_subfolder, keep_path=False)
+        self.copy("LICENSE.*", src=self.source_subfolder, keep_path=False)
+        self.copy("CHANGES.*", src=self.source_subfolder, keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hdf5", "hdf5_hl"]
-        if self.options.cxx:
-            self.cpp_info.libs.append("hdf5_cpp")
-        if tools.os_info.is_windows:
-            self.cpp_info.defines = ["H5_BUILT_AS_DYNAMIC_LIB"]
+        self.cpp_info.libs = tools.collect_libs(self)
+
+        if not self.options.shared and not self.settings.os == "Windows":
+            self.cpp_info.libs.append("dl")
